d7925128c1757703f949d1d66c7bdf7a9eb632bc
aroraChiraag <70032608+aroraChiraag@users.noreply.github.com>
2021-08-26 17:57:20 -0400

    Update Extraction elements with Text 03.py

--- Code Files/Extraction elements with Text 03.py
+++ Code Files/Extraction elements with Text 03.py
@@ -4,9 +4,9 @@ Created on Thu Jun 17 11:07:14 2021
 
 @author: ca273
 """
-# Import the xml traverser: install before importing
+# Import the xml traverser
 import xml.etree.ElementTree as ET
-# import dependcy for data structure
+# Import dependency for data structure
 import pandas as pd
 # Import dependency to load csv data and load location of current directory
 import os
@@ -26,7 +26,7 @@ for file in dirs:
 # Empty List to store the name of CSV files
 CSV_Name = []
 
-# Create names for CSV Files to be created 
+# Create names for CSV Files  
 for filename in XML_Files:
     CSV_Name.append(filename[:-4]+"_WithText.csv")
     
@@ -40,10 +40,10 @@ File_Number = int(input("Please choose a file number from the above list: "))
 # Load the parse from the Tree
 Form = ET.parse(XML_Files[File_Number])
 
-# get the root of the form tree
+# Get the root of the form tree
 root = Form.getroot()
 
-# demo variable to store the element attributes
+# Demo variable to store the element attributes
 name = []
 width = []
 height = []
@@ -81,6 +81,7 @@ for page in root.findall("LTPage"):
     for layer1 in page.findall("LTImage"):
         # Looking for Interactive elements within a Image
         for layer2 in layer1.findall("Annot"):
+            
             # Name of Interactive Element should not be None 
             if layer2.attrib.get("T") != None:
                     
@@ -120,10 +121,11 @@ for page in root.findall("LTPage"):
                     else:
                         types.append("checkbox")
                         
-            # Looking for Rectnagular Spaces within a Figures            
+            # Looking for Rectangular Spaces within a Figures            
             for layer3 in layer2.findall("LTRect"):
                 # Looking for Interactive elements within a Rectangular Spaces
                 for layer4 in layer3.findall("Annot"):    
+                    
                     # Name of Interactive Element should not be None 
                     if layer4.attrib.get("T") != None:
                         
@@ -147,6 +149,7 @@ for page in root.findall("LTPage"):
                     for layer5 in layer4.findall("LTFigure"):
                         # Looking for Interactive Elements within the Logical Figures
                         for layer6 in layer5.findall("Annot"):
+                            
                             # Name of Interactive Element should not be None 
                             if layer6.attrib.get("T") != None:
                                     
@@ -188,6 +191,7 @@ for page in root.findall("LTPage"):
 
     # Looking for Interactive elements within a page
     for layer1 in page.findall("Annot"):
+        
         # Name of Interactive Element should not be None 
         if layer1.attrib.get("T") != None:
                 
@@ -209,6 +213,7 @@ for page in root.findall("LTPage"):
     for layer1 in page.findall("LTRect"):
         # Looking for interactive spaces within those rectangular spaces
         for layer2 in layer1.findall("Annot"):
+            
             # Name of Interactive Element should not be None 
             if layer2.attrib.get("T") != None:
                 
@@ -230,6 +235,7 @@ for page in root.findall("LTPage"):
         for layer2 in layer1.findall("LTRect"):
             # Looking for interactive spaces within those rectangular spaces
             for layer3 in layer2.findall("Annot"):
+                
                 # Name of Interactive Element should not be None 
                 if layer3.attrib.get("T") != None:
                 
@@ -254,19 +260,22 @@ for page in root.findall("LTPage"):
             # Looking for Interactive elements within a Text Box
             for layer3 in layer2.findall("Annot"):
                 
-                name.append(layer3.attrib.get("T"))
-                width.append(layer3.attrib.get("width"))
-                height.append(layer3.attrib.get("height"))
-                x_0.append(layer3.attrib.get("x0"))
-                y_0.append(layer3.attrib.get("y0"))
-                x_1.append(layer3.attrib.get("x1"))
-                y_1.append(layer3.attrib.get("y1"))
+                # Name of Interactive Element should not be None 
+                if layer3.attrib.get("T") != None:
+                
+                    name.append(layer3.attrib.get("T"))
+                    width.append(layer3.attrib.get("width"))
+                    height.append(layer3.attrib.get("height"))
+                    x_0.append(layer3.attrib.get("x0"))
+                    y_0.append(layer3.attrib.get("y0"))
+                    x_1.append(layer3.attrib.get("x1"))
+                    y_1.append(layer3.attrib.get("y1"))
         
-                # Check if it's a blank text fillup or a checkbox
-                if layer3.attrib.get("FT") == "/Tx":
-                    types.append("text fillup")
-                else:
-                    types.append("checkbox")
+                    # Check if it's a blank text fillup or a checkbox
+                    if layer3.attrib.get("FT") == "/Tx":
+                        types.append("text fillup")
+                    else:
+                        types.append("checkbox")
     
     # Looking for Text Boxes within a page 
     for layer1 in page.findall("LTTextBoxHorizontal"):    
@@ -275,19 +284,22 @@ for page in root.findall("LTPage"):
             # Looking for interactive elements within Text Lines
             for layer3 in layer2.findall("Annot"):
                 
-                name.append(layer3.attrib.get("T"))
-                width.append(layer3.attrib.get("width"))
-                height.append(layer3.attrib.get("height"))
-                x_0.append(layer3.attrib.get("x0"))
-                y_0.append(layer3.attrib.get("y0"))
-                x_1.append(layer3.attrib.get("x1"))
-                y_1.append(layer3.attrib.get("y1"))
+                # Name of Interactive Element should not be None 
+                if layer3.attrib.get("T") != None:
+                
+                    name.append(layer3.attrib.get("T"))
+                    width.append(layer3.attrib.get("width"))
+                    height.append(layer3.attrib.get("height"))
+                    x_0.append(layer3.attrib.get("x0"))
+                    y_0.append(layer3.attrib.get("y0"))
+                    x_1.append(layer3.attrib.get("x1"))
+                    y_1.append(layer3.attrib.get("y1"))
         
-                # Check if it's a blank text fillup or a checkbox
-                if layer3.attrib.get("FT") == "/Tx":
-                    types.append("text fillup")
-                else:
-                    types.append("checkbox")
+                    # Check if it's a blank text fillup or a checkbox
+                    if layer3.attrib.get("FT") == "/Tx":
+                        types.append("text fillup")
+                    else:
+                        types.append("checkbox")
 
                            
     # Looking for Text Boxes within a page 
@@ -298,19 +310,22 @@ for page in root.findall("LTPage"):
             # Looking for interactive elements within Text Lines
             for layer3 in layer2.findall("Annot"):
                 
-                name.append(layer3.attrib.get("T"))
-                width.append(layer3.attrib.get("width"))
-                height.append(layer3.attrib.get("height"))
-                x_0.append(layer3.attrib.get("x0"))
-                y_0.append(layer3.attrib.get("y0"))
-                x_1.append(layer3.attrib.get("x1"))
-                y_1.append(layer3.attrib.get("y1"))
+                 # Name of Interactive Element should not be None 
+                if layer3.attrib.get("T") != None:
+                
+                    name.append(layer3.attrib.get("T"))
+                    width.append(layer3.attrib.get("width"))
+                    height.append(layer3.attrib.get("height"))
+                    x_0.append(layer3.attrib.get("x0"))
+                    y_0.append(layer3.attrib.get("y0"))
+                    x_1.append(layer3.attrib.get("x1"))
+                    y_1.append(layer3.attrib.get("y1"))
         
-                # Check if it's a blank text fillup or a checkbox
-                if layer3.attrib.get("FT") == "/Tx":
-                    types.append("text fillup")
-                else:
-                    types.append("checkbox")           
+                    # Check if it's a blank text fillup or a checkbox
+                    if layer3.attrib.get("FT") == "/Tx":
+                        types.append("text fillup")
+                    else:
+                        types.append("checkbox")           
     
     # Looking for Text Boxes within a page        
     for layer1 in page.findall("LTTextBoxHorizontal"):
